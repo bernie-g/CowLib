29add93a63d49e7fe9b89d1adaf405f696b321de
bernie-g <berniegandin@gmail.com>
2021-01-02 20:11:52 -0500

    Initial entity schema and factory setup

---
+++ CowLib/Factory/BaseFileFactory.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+
+namespace CowLib.Factory
+{
+    public abstract class BaseFileFactory<T>
+    {
+        private string name;
+        
+        public T builtObject { get; }
+        
+        public BaseFileFactory(string dragonEntity, T builtObject)
+        {
+            this.builtObject = builtObject;
+            this.name = dragonEntity;
+        }
+        
+
+        public string getName()
+        {
+            return this.name;
+        }
+        
+        public abstract void saveFile();
+    }
+}
---
+++ CowLib/Factory/EntityFactory.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Newtonsoft.Json;
+using QuickType;
+
+namespace CowLib.Factory
+{
+    public class EntityFactory : BaseFileFactory<EntityFile>
+    {
+        public EntityFactory(string dragonEntity, EntityFile entity = null) : base(dragonEntity, entity ?? new EntityFile())
+        {
+            
+        }
+
+        public override void saveFile()
+        {
+        }
+
+        public MinecraftEntity getEntity()
+        {
+            return builtObject.MinecraftEntity;
+        }
+
+        public Components getComponents()
+        {
+            return builtObject.MinecraftEntity.Components;
+        }
+
+        public Components createComponentGroup(string name)
+        {
+            Components components = new Components();
+            builtObject.MinecraftEntity.ComponentGroups.Add(name, components);
+            return components;
+        }
+        
+        public override string ToString()
+        {
+            return Serialize.ToJson(builtObject);
+        }
+    }
+}
